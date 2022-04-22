4d11bf6b821493ca15ff25050aef07afaa3d3dc7
PopsicleTreehouse <ahoynodnarb@gmail.com>
2022-04-21 19:32:24 -0700

    Removed useless files

--- Pods/Target Support Files/MarqueeLabel-ObjC/MarqueeLabel-ObjC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MarqueeLabel_ObjC : NSObject
-@end
-@implementation PodsDummy_MarqueeLabel_ObjC
-@end
--- Pods/Target Support Files/Pods-LyricsTester/Pods-LyricsTester-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LyricsTester : NSObject
-@end
-@implementation PodsDummy_Pods_LyricsTester
-@end
